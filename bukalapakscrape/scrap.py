8efd11db2ada965deb171b3f6018eb73beca728e
sumirart <akun.pratamasumirat@gmail.com>
2018-12-29 22:50:23 +0700

    scrape save to db

--- bukalapakscrape/scrap.py
+++ bukalapakscrape/scrap.py
@@ -1,21 +1,13 @@
 from requests import get
 from bs4 import BeautifulSoup as BS
 import json
+import MySQLdb
 
-# def getListBK(category,page):
-#     url = "https://bioskopkeren.fun/{}/page/{}".format(category, page)
-#     req = get(url).content
-#     page = BS(req, 'html.parser')
-#     for movies in page.find_all('div', 'filmcontent'):
-#         data = []
-#         for div in movies.find_all('div', 'moviefilm'):
-#             out = {}
-#             a = div.find('a', href=True)
-#             out.update({'url': a['href']})
-#             img = div.find('img')
-#             out.update({'thumbnails': img['src']})
-#             data.append(out)
-#     return(json.dumps(data))
+# SQL DB Config
+HOST = "127.0.0.1"
+USERNAME = "root"
+PASSWORD = ""
+DATABASE = "arkademyfinaltask"
 
 def getAll(page):
     url = "https://m.bukalapak.com/c/handphone?from=category_home&page={}&search%5Bkeywords%5D=".format(page)
@@ -26,15 +18,16 @@ def getAll(page):
     data = []
     for products in page.find_all('article', 'product-display'):
         out = {}
-        # title
+        # title - find tag name data-name inside article with class product-display
         title = products['data-name']
         out.update({'title': title})
 
         # link
         link = products['data-url']
-        out.update({'url': bukalapak + link })
+        url = bukalapak + link
+        out.update({'url': url })
 
-        # image
+        # image - find img tag inside article with class product-media__img and get the value of data-src attribute
         img = products.find('img', "product-media__img")['data-src']
         out.update({'thumbnails': img})
 
@@ -43,4 +36,23 @@ def getAll(page):
         out.update({'price': priceTag})
 
         data.append(out)
+
+        # SQL Connection, for saving to DB
+        db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
+        # prepare cursor object using cursor() method
+        cursor = db.cursor()
+        # prepare SQl query for inserting to DB
+        sql = "INSERT INTO products(name, price, image_url) VALUES ('{}', '{}', '{}')".format(title, priceTag, img, 'NOW()')
+        # sql = "INSERT INTO classes(name_of_class, type_of_course) VALUES ('{}', '{}')".format(name_of_class, type_of_course, 'NOW()')
+
+        try:
+            # Execute SQL command
+            cursor.execute(sql)
+            # commit changes in the db
+            db.commit()
+        except:
+            # rollback if there is any error
+            db.rollback()
+            db.close()
+            
     return(json.dumps(data))
