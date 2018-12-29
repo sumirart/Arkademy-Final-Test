024916b0a6353c89c2512540094a4b45b2203aac
sumirart <akun.pratamasumirat@gmail.com>
2018-12-29 22:21:14 +0700

    fix scrap

--- bukalapakscrape/scrap.py
+++ bukalapakscrape/scrap.py
@@ -2,68 +2,45 @@ from requests import get
 from bs4 import BeautifulSoup as BS
 import json
 
-def getListBK(category,page):
-    url = "https://bioskopkeren.fun/{}/page/{}".format(category, page)
-    req = get(url).content
-    page = BS(req, 'html.parser')
-    for movies in page.find_all('div', 'filmcontent'):
-        data = []
-        for div in movies.find_all('div', 'moviefilm'):
-            out = {}
-            a = div.find('a', href=True)
-            out.update({'url': a['href']})
-            img = div.find('img')
-            out.update({'thumbnails': img['src']})
-            out.update({'title': img['alt']})
-            data.append(out)
-    return(json.dumps(data))
+# def getListBK(category,page):
+#     url = "https://bioskopkeren.fun/{}/page/{}".format(category, page)
+#     req = get(url).content
+#     page = BS(req, 'html.parser')
+#     for movies in page.find_all('div', 'filmcontent'):
+#         data = []
+#         for div in movies.find_all('div', 'moviefilm'):
+#             out = {}
+#             a = div.find('a', href=True)
+#             out.update({'url': a['href']})
+#             img = div.find('img')
+#             out.update({'thumbnails': img['src']})
+#             data.append(out)
+#     return(json.dumps(data))
 
 def getAll(page):
-    url = "https://m.bukalapak.com/c/handphone?page={}".format(page)
+    url = "https://m.bukalapak.com/c/handphone?from=category_home&page={}&search%5Bkeywords%5D=".format(page)
+    bukalapak = "https://www.bukalapak.com"
     req = get(url).content
     page = BS(req, 'html.parser')
-    for movies in page.find_all('ul', 'products'):
-        data = []
-        for div in movies.find_all('li', 'product'):
-            out = {}
-            a = div.find('a', href=True)
-            out.update({'url': a['href']})
-            img = div.find('img')
-            out.update({'thumbnails': img['src']})
-            out.update({'title': img['alt']})
-            data.append(out)
-    return(json.dumps(data))
+    # for div in page.find_all('div', 'product-list'):
+    data = []
+    for products in page.find_all('article', 'product-display'):
+        out = {}
+        # title
+        title = products['data-name']
+        out.update({'title': title})
 
-# def getInfoBK(url):
-#     out = {}
-#     req = get(url).content
-#     bs = BS(req, 'html.parser')
-#     iframe = bs.find('iframe')
-#     out.update({'embed': iframe['src']})
-#     # div = bs.find('div','filmicerik')
-#     for div in bs.find_all('div', 'filmicerik'):
-#         desc = div.find_all('p')[2].find_next('span')
-#         out.update({'description': desc.text.strip('Sinopsis – ' or '\n')})
-#         break
-#     det = bs.find('div','filmaltiaciklama')
-#     for p in det.find_all('p'):
-#         data = []
-#         if p.find('span'):
-#             title = p.find('span').text
-#             if p.find('a'):
-#                 isi = [x.text for x in p.find_all('a')]
-#             else:
-#                 isi = p.contents[1][2:].strip()
-#             out.update({title : isi})
-#             data.append(out)
-#     for elemen in data:
-#         del elemen['Etiketler']
-#         return(json.dumps(data))
-    
-# def getEmbedBK(url):
-#     out = {}
-#     req = get(url).content
-#     bs = BS(req, 'html.parser')
-#     iframe = bs.find('iframe')
-#     out.update({'embed': iframe['src']})
-#     return(json.dumps(out))
+        # link
+        link = products['data-url']
+        out.update({'url': bukalapak + link })
+
+        # image
+        img = products.find('img', "product-media__img")['data-src']
+        out.update({'thumbnails': img})
+
+        # price
+        priceTag = products.find('div', "product-price")['data-reduced-price']
+        out.update({'price': priceTag})
+
+        data.append(out)
+    return(json.dumps(data))
